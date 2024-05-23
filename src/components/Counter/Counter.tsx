fab2453fb4d7741e92921851e8f78690297cfa12
Mukhammadamin <mukhammadamin_sultonov@epam.com>
2024-05-23 11:28:45 +0500

    refactor: code refactor

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -6,37 +6,32 @@ interface ICounterProps {
 }
 
 class Counter extends React.Component<ICounterProps> {
-    state = {
-        count: this.props.initValue,
-      };
+  state = {
+    count: this.props.initValue,
+  };
 
-      increment = () => {
-        this.setState({
-            count: this.state.count + 1
-        })
-      }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
 
-      decrement = () => {
-        this.setState({
-            count: this.state.count - 1
-        })
-      }
-    
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
 
   render() {
-    return (<article className={cl.counter}>
-    <h2>
-      {this.state.count}
-    </h2>
-    <div className={cl.counterControls}>
-      <button onClick={this.increment}>
-        Plus
-      </button>
-      <button onClick={this.decrement}>
-        Minus
-      </button>
-    </div>
-  </article>);
+    return (
+      <article className={cl.counter}>
+        <h2>{this.state.count}</h2>
+        <div className={cl.counterControls}>
+          <button onClick={this.increment}>Plus</button>
+          <button onClick={this.decrement}>Minus</button>
+        </div>
+      </article>
+    );
   }
 }
 

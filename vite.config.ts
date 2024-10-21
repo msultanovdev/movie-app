efad13c67a36a9bc14c9f85d6dd71935fe1917f6
Mukhammadamin <mukhammadamin.sultonov@litmos.com>
2024-10-22 02:38:45 +0500

    setup test & write tests for counter component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+import type { UserConfig as VitestUserConfigInterface } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+};
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  test: vitestConfig.test,
 });

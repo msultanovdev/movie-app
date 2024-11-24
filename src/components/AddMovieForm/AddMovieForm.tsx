6a47ecb6d9a25475c114942babe148af42a19472
Mukhammadamin <mukhammadamin.sultonov@litmos.com>
2024-11-24 10:19:31 +0500

    feat: add movie function

--- src/components/AddMovieForm/AddMovieForm.tsx
+++ src/components/AddMovieForm/AddMovieForm.tsx
@@ -1,9 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import Dialog from "../Dialog/Dialog";
 import MovieForm from "../MovieForm/MovieForm";
+import { IMovie } from "../../types";
+import axios from "axios";
+import { moviesURL } from "../../consts";
 
 const AddMovieForm = () => {
   const navigate = useNavigate();
+
+  const handleSubmit = async (body: IMovie) => {
+    try {
+      const { data } = await axios.post<IMovie>(moviesURL, body);
+      navigate(`/${data.id}`);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <Dialog
@@ -12,7 +24,7 @@ const AddMovieForm = () => {
       }}
       title="Add Movie"
     >
-      <MovieForm onSubmit={() => {}} />
+      <MovieForm onSubmit={handleSubmit} />
     </Dialog>
   );
 };
--- src/components/MovieForm/MovieForm.tsx
+++ src/components/MovieForm/MovieForm.tsx
@@ -1,90 +1,135 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import "./MovieForm.css";
 import { IMovie } from "../../types";
 import FlexRow from "../../layouts/FlexRow/FlexRow";
-import { formatDate } from "../../utils/helper";
+// import { formatDate } from "../../utils/helper";
+import { Controller, useForm } from "react-hook-form";
+import Select, { Options } from "react-select";
 
 export interface IMovieFormProps {
   initiaMovieState?: IMovie;
   onSubmit: (data: IMovie) => void;
 }
 
-const epmtyMovieState: IMovie = {
-  id: Date.now() + Math.floor(Math.random() * 1000),
-  title: "",
-  release_date: formatDate(new Date()),
-  poster_path: "",
-  vote_average: "",
-  genres: [],
-  runtime: "",
-  overview: "",
-  tagline: "",
-  vote_count: "",
-  budget: "",
-  revenue: "",
-};
+// const epmtyMovieState: IMovie = {
+//   id: Date.now() + Math.floor(Math.random() * 1000),
+//   title: "",
+//   release_date: formatDate(new Date()),
+//   poster_path: "",
+//   vote_average: "",
+//   genres: [],
+//   runtime: "",
+//   overview: "",
+//   tagline: "",
+//   vote_count: "",
+//   budget: "",
+//   revenue: "",
+// };
+
+type SelectOptionType = Options<{ value: string; label: string }>;
+
+const options: SelectOptionType = [
+  { value: "Action", label: "Action" },
+  { value: "Adventure", label: "Adventure" },
+  { value: "Comedy", label: "Comedy" },
+  { value: "Documentary", label: "Documentary" },
+];
 
 const MovieForm: FC<IMovieFormProps> = (props) => {
-  const { initiaMovieState, onSubmit } = props;
-  const [formData, setFormData] = useState<IMovie>(
-    initiaMovieState ?? epmtyMovieState
-  );
-  const handleChange = (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-    >
-  ) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    reset,
+    setValue,
+  } = useForm<IMovie<SelectOptionType>>();
+  const { onSubmit } = props;
+  // const [formData, setFormData] = useState<IMovie>(
+  //   initiaMovieState ?? epmtyMovieState
+  // );
+
+  // const handleChange = (
+  //   e: React.ChangeEvent<
+  //     HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+  //   >
+  // ) => {
+  //   const { name, value } = e.target;
+  //   setFormData({ ...formData, [name]: value });
+  // };
+
+  // const handleGenreChange = (
+  //   selectedOptions: { value: string; label: string }[]
+  // ) => {
+  //   const genreLabels = selectedOptions.map((option) => option.label);
+  //   setValue("genres", genreLabels);
+  // };
+
   const handleReset = () => {
-    setFormData({ ...epmtyMovieState, id: formData.id });
+    reset();
+    setValue("genres", []);
   };
+
+  const onFormSubmit = (data: IMovie<SelectOptionType>) => {
+    const updatedData = {
+      ...data,
+      genres: data.genres.map((option: { label: string }) => option.label),
+    };
+    onSubmit(updatedData);
+  };
+
   return (
-    <form className="movie-form" onSubmit={() => onSubmit(formData)}>
+    <form className="movie-form" onSubmit={handleSubmit(onFormSubmit)}>
       <FlexRow>
         <div className="form-group form-full-input">
-          <label>Title</label>
+          <label>Title {errors.title && "is required"}</label>
           <input
+            {...register("title", { required: true })}
             type="text"
             name="title"
-            value={formData.title}
-            onChange={handleChange}
             placeholder="Moana"
           />
         </div>
 
         <div className="form-group">
-          <label>Release Date</label>
+          <label>Release Date {errors.release_date && "is required"}</label>
           <input
+            {...register("release_date", { required: true })}
             type="date"
             name="release_date"
-            value={formatDate(new Date(formData.release_date))}
-            onChange={handleChange}
           />
         </div>
       </FlexRow>
 
       <FlexRow>
         <div className="form-group form-full-input">
-          <label>Movie URL</label>
+          <label>
+            Movie URL {errors.poster_path && "must follow url pattern"}
+          </label>
           <input
-            type="url"
+            {...register("poster_path", {
+              required: true,
+              pattern: /https?:\/\/[^\s/$.?#].[^\s]*/i,
+            })}
             name="poster_path"
-            value={formData.poster_path}
-            onChange={handleChange}
             placeholder="https://"
           />
         </div>
 
         <div className="form-group">
-          <label>Rating</label>
+          <label>
+            Rating {errors.vote_average && "must be between 0 - 10"}
+          </label>
           <input
+            {...register("vote_average", {
+              required: true,
+              min: 0,
+              max: 10,
+              valueAsNumber: true,
+            })}
             type="number"
             name="vote_average"
             step="0.1"
-            value={formData.vote_average}
-            onChange={handleChange}
             placeholder="7.8"
           />
         </div>
@@ -92,26 +137,31 @@ const MovieForm: FC<IMovieFormProps> = (props) => {
 
       <FlexRow>
         <div className="form-group form-full-input">
-          <label>Genre</label>
-          <select
-            name="genre"
-            value={formData.genres[0]}
-            onChange={handleChange}
-          >
-            <option value="">Select Genre</option>
-            <option value="Action">Action</option>
-            <option value="Adventure">Adventure</option>
-            <option value="Comedy">Comedy</option>
-          </select>
+          <label>Genre {errors.genres && "select at least one genre"}</label>
+          <Controller
+            name="genres"
+            control={control}
+            render={({ field }) => {
+              return (
+                <Select
+                  {...register("genres", { required: true })}
+                  {...field}
+                  options={options}
+                  isMulti
+                  className="react-select-container"
+                  classNamePrefix="react-select"
+                />
+              );
+            }}
+          />
         </div>
 
         <div className="form-group">
           <label>Runtime</label>
           <input
+            {...register("runtime", { required: true, valueAsNumber: true })}
             type="number"
             name="runtime"
-            value={formData.runtime}
-            onChange={handleChange}
             placeholder="minutes"
           />
         </div>
@@ -120,9 +170,8 @@ const MovieForm: FC<IMovieFormProps> = (props) => {
       <div className="form-group overview">
         <label>Overview</label>
         <textarea
+          {...register("overview", { required: true })}
           name="overview"
-          value={formData.overview}
-          onChange={handleChange}
           placeholder="Movie description"
         />
       </div>
